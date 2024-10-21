5f3ecf7be88b9bcab8e4e3c9f982a9f4030b3c5c
Gil Fernandes <gil.fernandes@onepointltd.com>
2024-10-21 10:27:11 +0100

    Added chunks with overlap function

--- data_wellness_hackathon/text_splitter.py
+++ data_wellness_hackathon/text_splitter.py
@@ -1,14 +1,7 @@
-from typing import Iterator, Tuple
+from typing import Iterator, Tuple, Callable
 from pathlib import Path
 
 from data_wellness_hackathon.config import cfg
-
-
-def split_text(maxlength: int = 100) -> Iterator[str]:
-    for doc in cfg.doc_location.glob("*.txt"):
-        text = doc.read_text(encoding="utf-8").strip()
-        for chunk in get_chunks(doc, text, maxlength):
-            yield chunk
 
 
 def get_chunks(doc: Path, s: str, maxlength: int) -> Iterator[Tuple[str, str]]:
@@ -21,7 +14,29 @@ def get_chunks(doc: Path, s: str, maxlength: int) -> Iterator[Tuple[str, str]]:
     yield doc.stem, s[start:]
 
 
+def get_chunks_overlap(doc: Path, s: str, maxlength: int, slide_back_divisor: int = 10) -> Iterator[Tuple[str, str]]:
+    start = 0
+    end = 0
+    splits = s.split()
+    split_len = len(splits)
+    while start - maxlength // slide_back_divisor < split_len:
+        end = start + maxlength + 1
+        yield doc.stem, " ".join(splits[start:end])
+        start = end + 1 - maxlength // slide_back_divisor # slide back by fraction of text
+
+
+def split_text(maxlength: int = 100, chunks: Callable = get_chunks) -> Iterator[str]:
+    for doc in cfg.doc_location.glob("*.txt"):
+        text = doc.read_text(encoding="utf-8").strip()
+        for chunk in chunks(doc, text, maxlength):
+            yield chunk
+
+
 if __name__ == "__main__":
-    for t in split_text(2048):
+
+    def get_chunks_split_20(doc: Path, s: str, maxlength: int):
+        return get_chunks_overlap(doc, s, maxlength, 20)
+
+    for t in split_text(200, get_chunks_split_20):
         print(" *************************** ")
         print(t)
